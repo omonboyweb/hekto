be14d23c2c06c4f7221d8f08a6311b030257b549
omonboyweb <xojamurodov404@gmail.com>
2025-06-27 16:33:45 +0500

    deployed

---
+++ json.js
@@ -0,0 +1,69 @@
+export const category = [
+    "New Arrival",
+    "Best Seller",
+    "Featured",
+    "Special Offer"
+];
+export const products = [
+    {
+        title: "New Arrival",
+        product: [
+            {
+                name: "Comfort Handy Craft",
+                img: "https://ibb.co/Mxvr8kcq",
+                priceSale: 42.00,
+                totalPrice: 65.00
+            }, {
+                name: "Comfort Handy Craft  ",
+                img: "https://ibb.co/JRgsY5TW",
+                priceSale: 42.00,
+                totalPrice: 65.00
+            },
+            {
+                name: "Comfort Handy Craft  ",
+                img: "https://ibb.co/kgwdWNxm",
+                priceSale: 42.00,
+                totalPrice: 65.00
+            },
+            {
+                name: "Comfort Handy Craft  ",
+                img: "https://ibb.co/zT3C782R",
+                priceSale: 42.00,
+                totalPrice: 65.00
+            }
+        ]
+    },
+    {
+        title: "Best Seller",
+        product: [
+            {
+                name: "Comfort Handy Craft  ",
+                img: "https://ibb.co/JRgsY5TW",
+                priceSale: 42.00,
+                totalPrice: 65.00
+            }
+        ]
+    },
+    {
+        title: "Featured",
+        product: [
+            {
+                name: "Comfort Handy Craft  ",
+                img: "https://ibb.co/kgwdWNxm",
+                priceSale: 42.00,
+                totalPrice: 65.00
+            }
+        ]
+    },
+    {
+        title: "Special Offer",
+        product: [
+            {
+                name: "Comfort Handy Craft  ",
+                img: "https://ibb.co/zT3C782R",
+                priceSale: 42.00,
+                totalPrice: 65.00
+            }
+        ]
+    }
+];
