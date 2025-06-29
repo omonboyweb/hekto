872ea4f0d9db89585491835e56307dde47f05837
omonboyweb <xojamurodov404@gmail.com>
2025-06-29 19:55:37 +0500

    added wishlist menu add remove and add tab menues

--- json.js
+++ json.js
@@ -13,41 +13,47 @@ export const products = [
                 name: "Comfort Handy Craft",
                 img: "https://olcha.uz/image/original/products//supplier/stores/27263/j501%2Fj501-photoroom-1.jpg",
                 priceSale: 42.00,
-                totalPrice: 65.00
+                totalPrice: 65.00,
+                wishlist: false
             }, {
                 id: 2,
                 name: "Comfort Handy Craft  ",
                 img: "https://olcha.uz/image/original/products//supplier/stores/27263/j501%2Fj501-photoroom-1.jpg",
                 priceSale: 42.00,
-                totalPrice: 65.00
+                totalPrice: 65.00,
+                wishlist: true
             },
             {
                 id: 3,
                 name: "Comfort Handy Craft  ",
                 img: "https://olcha.uz/image/original/products//supplier/stores/27263/j501%2Fj501-photoroom-1.jpg",
                 priceSale: 42.00,
-                totalPrice: 65.00
+                totalPrice: 65.00,
+                wishlist: false
             },
             {
                 id: 4,
                 name: "Comfort Handy Craft  ",
                 img: "https://olcha.uz/image/original/products//supplier/stores/27263/j501%2Fj501-photoroom-1.jpg",
                 priceSale: 42.00,
-                totalPrice: 65.00
+                totalPrice: 65.00,
+                wishlist: false
             },
             {
                 id: 5,
                 name: "Comfort Handy Craft",
                 img: "https://olcha.uz/image/original/products//supplier/stores/27263/j501%2Fj501-photoroom-1.jpg",
                 priceSale: 42.00,
-                totalPrice: 65.00
+                totalPrice: 65.00,
+                wishlist: false
             },
             {
                 id: 6,
                 name: "Comfort Handy Craft",
                 img: "https://olcha.uz/image/original/products//supplier/stores/27263/j501%2Fj501-photoroom-1.jpg",
                 priceSale: 42.00,
-                totalPrice: 65.00
+                totalPrice: 65.00,
+                wishlist: false
             }
         ]
     },
@@ -55,44 +61,50 @@ export const products = [
         title: "Best Seller",
         product: [
             {
-                id: 1,
+                id: 7,
                 name: "Comfort Handy Craft  ",
                 img: "https://www.mebelchi.shop/uploads/products/32200f78-ffa9-4b10-afa6-71bd3d9d3499.jpg",
                 priceSale: 42.00,
-                totalPrice: 65.00
+                totalPrice: 65.00,
+                wishlist: false
             },
             {
-                id: 2,
+                id: 8,
                 name: "Comfort Handy Craft  ",
                 img: "https://www.mebelchi.shop/uploads/products/973cf14f-25e1-4b9a-b90a-af31ba5ed25d.png",
                 priceSale: 42.00,
-                totalPrice: 65.00
+                totalPrice: 65.00,
+                wishlist: false
             },
             {
-                id: 3,
+                id: 9,
                 name: "Comfort Handy Craft  ",
                 img: "https://www.mebelchi.shop/uploads/products/973cf14f-25e1-4b9a-b90a-af31ba5ed25d.png",
                 priceSale: 42.00,
-                totalPrice: 65.00
+                totalPrice: 65.00,
+                wishlist: false
             },
             {
-                id: 4,
+                id: 10,
                 name: "Comfort Handy Craft  ",
                 img: "https://www.mebelchi.shop/uploads/products/973cf14f-25e1-4b9a-b90a-af31ba5ed25d.png",
                 priceSale: 42.00,
-                totalPrice: 65.00
+                totalPrice: 65.00,
+                wishlist: false
             }, {
-                id: 5,
+                id: 11,
                 name: "Comfort Handy Craft  ",
                 img: "https://www.mebelchi.shop/uploads/products/32200f78-ffa9-4b10-afa6-71bd3d9d3499.jpg",
                 priceSale: 42.00,
-                totalPrice: 65.00
+                totalPrice: 65.00,
+                wishlist: false
             }, {
-                id: 2,
+                id: 12,
                 name: "Comfort Handy Craft  ",
                 img: "https://www.mebelchi.shop/uploads/products/973cf14f-25e1-4b9a-b90a-af31ba5ed25d.png",
                 priceSale: 42.00,
-                totalPrice: 65.00
+                totalPrice: 65.00,
+                wishlist: false
             }
         ]
     },
@@ -106,39 +118,44 @@ export const products = [
                 priceSale: 42.00,
                 totalPrice: 65.00
             }, {
-                id: 2,
+                id: 13,
                 name: "Comfort Handy Craft  ",
                 img: "https://ibb.co/JRgsY5TW",
                 priceSale: 42.00,
-                totalPrice: 65.00
+                totalPrice: 65.00,
+                wishlist: false
             },
             {
-                id: 3,
+                id: 14,
                 name: "Comfort Handy Craft  ",
                 img: "https://ibb.co/kgwdWNxm",
                 priceSale: 42.00,
-                totalPrice: 65.00
+                totalPrice: 65.00,
+                wishlist: false
             },
             {
-                id: 4,
+                id: 15,
                 name: "Comfort Handy Craft  ",
                 img: "https://ibb.co/zT3C782R",
                 priceSale: 42.00,
-                totalPrice: 65.00
+                totalPrice: 65.00,
+                wishlist: false
             },
             {
-                id: 5,
+                id: 16,
                 name: "Comfort Handy Craft",
                 img: "https://ibb.co/SX3sdYXt",
                 priceSale: 42.00,
-                totalPrice: 65.00
+                totalPrice: 65.00,
+                wishlist: false
             },
             {
-                id: 6,
+                id: 17,
                 name: "Comfort Handy Craft",
                 img: "https://ibb.co/HDYyRPfx",
                 priceSale: 42.00,
-                totalPrice: 65.00
+                totalPrice: 65.00,
+                wishlist: false
             }
         ]
     },
@@ -146,44 +163,50 @@ export const products = [
         title: "Special Offer",
         product: [
             {
-                id: 1,
+                id: 18,
                 name: "Comfort Handy Craft  ",
                 img: "https://www.mebelchi.shop/uploads/products/32200f78-ffa9-4b10-afa6-71bd3d9d3499.jpg",
                 priceSale: 42.00,
-                totalPrice: 65.00
+                totalPrice: 65.00,
+                wishlist: false
             },
             {
-                id: 2,
+                id: 19,
                 name: "Comfort Handy Craft  ",
                 img: "https://www.mebelchi.shop/uploads/products/973cf14f-25e1-4b9a-b90a-af31ba5ed25d.png",
                 priceSale: 42.00,
-                totalPrice: 65.00
+                totalPrice: 65.00,
+                wishlist: false
             },
             {
-                id: 3,
+                id: 20,
                 name: "Comfort Handy Craft  ",
                 img: "https://www.mebelchi.shop/uploads/products/973cf14f-25e1-4b9a-b90a-af31ba5ed25d.png",
                 priceSale: 42.00,
-                totalPrice: 65.00
+                totalPrice: 65.00,
+                wishlist: false
             },
             {
-                id: 4,
+                id: 21,
                 name: "Comfort Handy Craft  ",
                 img: "https://www.mebelchi.shop/uploads/products/973cf14f-25e1-4b9a-b90a-af31ba5ed25d.png",
                 priceSale: 42.00,
-                totalPrice: 65.00
+                totalPrice: 65.00,
+                wishlist: false
             }, {
-                id: 5,
+                id: 22,
                 name: "Comfort Handy Craft  ",
                 img: "https://www.mebelchi.shop/uploads/products/32200f78-ffa9-4b10-afa6-71bd3d9d3499.jpg",
                 priceSale: 42.00,
-                totalPrice: 65.00
+                totalPrice: 65.00,
+                wishlist: false
             }, {
-                id: 2,
+                id: 23,
                 name: "Comfort Handy Craft  ",
                 img: "https://www.mebelchi.shop/uploads/products/973cf14f-25e1-4b9a-b90a-af31ba5ed25d.png",
                 priceSale: 42.00,
-                totalPrice: 65.00
+                totalPrice: 65.00,
+                wishlist: false
             }
         ]
     },
--- main.js
+++ main.js
@@ -15,9 +15,12 @@ const getStorege = (key) => {
   return data
 }
 
+let newArr = getStorege("wishlist") || [];
+
+
 // Bu yerda saytda bir marta ishlatiladigan kodlar yozilgan buning vazifasi localStrogedan thame statusini olish uchun ishlatilyabdi
 (() => {
-  const theme = getStorege("theme")
+  const theme = getStorege("theme");
   if (theme) {
     body.classList.add(theme)
   }
@@ -40,18 +43,20 @@ const render = () => {
 }
 render()
 /// Tanlangan productlarni html ga chizish uchun ishlatilyabdi
+let data;
 const renderProduct = (key) => {
   const found = products.find((item) => item.title === key);
   // bu yerda find elementni tekshiryabdi
   if (!found) return;
-  const data = found.product
+  data = found.product
   leatesCard.innerHTML = data.map((item) => {
     return `
-    <a href="#">
+    <a href="#" class="addEvent">
 <div class="leates__card">
   <img src="${item.img}" alt="Leates category img" />
                 <div class="leates__card-content">
                   <span class="leates__card-title">${item.name}</span>
+                  <span class="addWish" data-key="${item.id}">+</span>
                   <span class="leates__card-cost"
                     >$${item.priceSale} <span class="sale">$${item.totalPrice}</span></span
                   >
@@ -60,15 +65,70 @@ const renderProduct = (key) => {
   </a>
 `
   }).join("")
-}
+
+  updateWishlistClass()
+};
 ///Bu yerda tabdagi buttonlarni bosganda osha malumotlar  chiqishi uchun  ishlatilyabdi
 const findClick = function () {
   leatesCategory.addEventListener("click", (e) => {
     const targetBtn = e.target.getAttribute("data-key");
+    if (!targetBtn) return;
+    document.querySelectorAll(".category__item").forEach(el => el.classList.remove("tabActive"));
+    e.target.classList.add("tabActive");
+
     renderProduct(targetBtn)
   })
 }
 findClick()
+// Buyerda bosilgan elementga active class qo'shiladi
+
 
 render()
-renderProduct(category[0])
+renderProduct(category[0]);
+/// bu yerda bosilgan elementlarni newArr ga push qiladi
+
+leatesCard.addEventListener("click", (e) => {  /// bu yerda funcsiya event bo'lganda ishlaydi
+  const wishBtn = e.target.closest(".addWish"); /// bosilgan elementni tanib olyabdi
+  if (!wishBtn) return; /// egar boshqa joyni bosgan bo'lsa ishlamaydi bu
+  const id = wishBtn.getAttribute("data-key");   // data keyini olib olyabdi bu takrorlanmas qiymat
+  // ID wishlistda borligini tekshiramiz
+  const alreadyExists = newArr.some(obj => obj.id == id);
+
+  if (alreadyExists) {
+    ///borbo'lsa ochiradi 
+    newArr = newArr.filter(obj => obj.id != id);
+    wishBtn.classList.remove("wishActive");
+  } else {
+    //yo'q bo'lsa qo'shamiz
+    newArr.push({ id });
+    wishBtn.classList.add("wishActive");
+  }
+  saveStorege("wishlist", newArr);
+
+  // wishBtn.classList.add("wishActive"); ///  bu active class qo'shyabdi 
+  // newArr.push({ id }) /// bu yerda bosilgan elementni arrayga push qilyabdi yani keyinchalik ishlashi uchun ham
+  // saveStorege("wishlist", newArr); ///local storega saqlab qo'yyabdi 
+  // if (e.target.classList === "wishActive") {
+  //   newArr = newArr.filter((obj) => obj.id !== Number(e.target.getAttribute("data-key")));
+  //   wishBtn.classList.remove("wishActive");
+  //   render()
+  // }
+
+});
+function updateWishlistClass() {
+  const addWish = document.querySelectorAll(".addWish")
+  for (let i = 0; i < newArr.length; i++) {
+    const arrId = newArr[i].id;
+    for (let a = 0; a < data.length; a++) {
+      const defArrId = data[a].id;
+      if (defArrId == arrId) {
+        addWish.forEach(btn => {
+          if (btn.getAttribute("data-key") == defArrId) {
+            btn.classList.add("wishActive")
+          }
+        })
+      }
+    }
+  }
+
+};
